0cab587e54c4e198c9746a03844899b3c5131395
chendi <chendi03@58ganji.com>
2016-09-08 14:12:03 +0800

    update doc

--- README.md
+++ README.md
@@ -1,46 +1,37 @@
-npm install
-
-cd build
-
-node webpack.dev.js --target 0/index
 
 
+初始化
 
-node node webpack.dev.js --global
+>推荐node version>4.2.0
 
-node node webpack.dev.js --force --watch
-
-
-更新8/30
-
-npm install
-
-npm run build  全局打包
-
-npm run server 启动服务器
-
-localhost:9000/0  进入入口0中的index.html
-localhost:9000/1  进入入口1中的index.html
+>npm install
+>
+>
 
 
-
-##重构
-
-构建所有入口
->./build/dev_cli.js buildAll
 
 构建dll库
 >./build/dev_cli.js buildDll
 
+
+构建所有入口
+>./build/dev_cli.js buildAll
+
+
 构建指定的入口
->./build/dev_cli.js build --target
+>./build/dev_cli.js build --target [entry]
+>
+>./build/dev_cli.js build --target app/index
 
 支持热替换react的开发服务.
->./build/dev_cli.js dev --target
+>./build/dev_cli.js dev --target [entry]
 
 添加入口
->./build/dev_cli.js [dir]
+>./build/dev_cli.js create [dir]
 
+>以上命令都不会主动重新构建dll,可执行以下命令
+>
+>./build/dev_cli.js buildDll && ./build/dev_cli.js buid --target app/index
 
 ##CNPM
 
--- build/config.js
+++ build/config.js
@@ -4,7 +4,7 @@ module.exports = {
      outputPath: path.join(__dirname,'../','dist'),
      buildPath:path.join(__dirname),
      // publicPath:'http://public.chendi.cn/webpack/project/dist/',
-     publicPath:__HOT__?'http://localhost:9090/':'http://public.chendi.cn/webpack-project1/dist/',
+     publicPath:__HOT__?'http://localhost:9090/':'[your dist host]'//like 'http://public.chendi.cn/webpack-project1/dist/',
      sourcePath: path.join(__dirname,'../','src'),
      cachePath: path.join(__dirname,'../','.cache'),
      libPath: path.join(__dirname,'../src','lib'),
@@ -28,29 +28,11 @@ module.exports = {
           "none":true,// (or false) output nothing
           "errors-only":true,// only output when errors happen
           "minimal":true,// only output when errors or a new compilation happen
-          // "normal":true,// (or true) standard output
           "verbose":true,// output all the information webpack has
-          // "context":true,// (string) context directory for request shortening
-          // "hash":true,// add the hash of the compilation
-          // "version":true,// add webpack version information
           "timings":true,// add timing information
-          // "assets":true,// add assets information
           "chunks":false,// add chunk information (setting this to false allows for a less verbose output)
-          // "chunkModules":true,// add built modules information to chunk information
-          // "modules":true,// add built modules information
-          // "children":true,// add children information
-          // "cached":true,// add also information about cached (not built) modules
-          // "reasons":true,// add information about the reasons why modules are included
-          // "source":true,// add the source code of modules
-          // "errorDetails":true,// add details to errors (like resolving log)
-          // "chunkOrigins":true,// add the origins of chunks and chunk merging info
-          // "modulesSort":true,// (string) sort the modules by that field
-          // "chunksSort":true,// (string) sort the chunks by that field
-          // "assetsSort":true,// (string) sort the assets by that field
           "colors":true,
      },
-     // url-loader-limit:8000
-     //entry:['./0/1-1/index.js','./0/2-3/index.js'],
      // devtool:"#source-map",
      devtool:"#cheap-module-eval-source-map",
 };
